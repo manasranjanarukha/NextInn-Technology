3e78f2bc3828a4b3d1b4622996b522f4042e20ee
MANAS RANJAN ARUKHA <140185268+manasranjanarukha@users.noreply.github.com>
2025-08-03 21:10:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Nextinn_Technology/",
+  base: "/NextInn_Technology/",
   plugins: [react()],
   server: {
     host: true, // <-- This allows LAN access
